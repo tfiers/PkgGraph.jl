17735a282d233148419973f55ed0caeff6ac7fc5
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-02 03:04:23 +0000

    comments on PkgTemplate's output

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # PkgDepGraph
+<!-- Add .jl, djeez -->
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://tfiers.github.io/PkgDepGraph.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://tfiers.github.io/PkgDepGraph.jl/dev/)
 [![Build Status](https://github.com/tfiers/PkgDepGraph.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/tfiers/PkgDepGraph.jl/actions/workflows/CI.yml?query=branch%3Amain)
+<!-- No 'dev' docs badge please. Also is 'latest commit' or sth not better. And 'Stable' rather just the last release version!!
+God. 
+And a "build status" button? blegh.
+Hm no ok, it's fine, it's useful: did tests of last commit succeed (and doc build).
+But then don't call it Build youknow.
+ehwell, fine.
+maybe "CI"
+or "Test & build docs | last commit".
+or just fuckin nothing (although hm no, a shortcut is useful).
+
+in any case, i had this nicely in prev repo's.
+A nice documentation badge with a book somewhere.
+And sth about last commit test success or the like as well.
+-->
--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,37 @@
 using PkgDepGraph
 using Documenter
+# I want newest Documenter (gh logo repo link :))
+# https://github.com/JuliaDocs/Documenter.jl/blob/master/CHANGELOG.md
+# Manifest is committed so fine to do once.
+
 
 DocMeta.setdocmeta!(PkgDepGraph, :DocTestSetup, :(using PkgDepGraph); recursive=true)
+# This is from here: https://github.com/JuliaCI/PkgTemplates.jl/issues/232
+# But I didn't have any trouble with running doctests before?
 
 makedocs(;
     modules=[PkgDepGraph],
     authors="Tomas Fiers <tomas.fiers@gmail.com> and contributors",
+    # Is this needed? Where?
     repo="https://github.com/tfiers/PkgDepGraph.jl/blob/{commit}{path}#{line}",
+    # ↪ makedocs does this automatically for a gh remote
     sitename="PkgDepGraph.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
+        # ↪ when local, no /pagename/index.html. ok ig.
         canonical="https://tfiers.github.io/PkgDepGraph.jl",
         edit_link="main",
-        assets=String[],
+        footer=nothing,  # yee
     ),
     pages=[
         "Home" => "index.md",
     ],
+    # doctest is true by default
+    # Is doctest not better in test. For fast doc build feedback loop.
 )
 
 deploydocs(;
     repo="github.com/tfiers/PkgDepGraph.jl",
     devbranch="main",
 )
+# Shouldn't this only be done in CI.
