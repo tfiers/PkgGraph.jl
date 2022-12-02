da1dc6aab363f9b944a4f816c8c76bdd15e7e07b
Documenter.jl <documenter@juliadocs.github.io>
2022-12-02 01:10:22 +0000

    build based on 0992bf3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PkgDepGraph","category":"page"},{"location":"#PkgDepGraph","page":"Home","title":"PkgDepGraph","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PkgDepGraph.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PkgDepGraph]","category":"page"}]
+}
