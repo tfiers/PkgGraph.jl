b3b8c91983e59e37823b7636f67390a59982e0ed
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-10 03:01:12 +0000

    doctest: ah duh, instantiate before import

--- docs/doctest.jl
+++ docs/doctest.jl
@@ -1,11 +1,11 @@
-using Documenter
-using PkgGraph
-
 on_github = (get(ENV, "CI", "") == "true")  # This is set by GitHub itself.
 
 if on_github
     using Pkg
     Pkg.instantiate()
 end
+
+using Documenter
+using PkgGraph
 
 doctest(PkgGraph)
