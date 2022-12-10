f94d6363bb39ad9bc7bbd9f2179159d6130f85e5
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-10 03:46:32 +0000

    update tests for new names; shorten `*url*` names

--- src/online.jl
+++ src/online.jl
@@ -11,28 +11,28 @@ online Graphviz renderer), use [`create`](@ref).
 For more info, see [`depgraph_url`](@ref).
 """
 open(pkgname) = begin
-    DefaultApplication.open(depgraph_url(pkgname))
+    DefaultApplication.open(url(pkgname))
     # ↪ Passing a url opens the browser on all platforms. (Even though that is undocumented:
     #   https://github.com/tpapp/DefaultApplication.jl/issues/12)
     return nothing
 end
 
-const rendering_urls = [
+const base_urls = [
     "https://dreampuf.github.io/GraphvizOnline/#",     # Default
     "http://magjac.com/graphviz-visual-editor/?dot=",  # Linked from graphviz.org. Many features.
     "https://edotor.net/?engine=dot#",
 ]
-const rendering_url = Ref(first(rendering_urls))
+const base_url = Ref(first(base_urls))
 
 """
-    set_rendering_url(new)
+    set_base_url(new)
 
-Set the base url that will be used by [`open`](@ref) and [`depgraph_url`](@ref) to the
-given `new` url.
+Set the rendering website that will be used by [`open`](@ref) and [`depgraph_url`](@ref) to
+the given `new` url.
 
-See `PkgGraph.rendering_urls` for some options.
+See `PkgGraph.base_urls` for some options.
 """
-set_rendering_url(new) = (rendering_url[] = new)
+set_base_url(new) = (base_url[] = new)
 
 
 """
@@ -44,10 +44,7 @@ online Graphviz rendering service.
 ## How it works
 The dependency graph of `pkgname` is created locally, and converted to a string in the
 Graphviz DOT format (see [`deps_as_DOT`](@ref)). This string is URL-encoded, and appended to
-a partly-complete URL, which is by default the first entry in the `PkgGraph.rendering_urls`
-list. To use a different rendering website, use [`set_rendering_url`](@ref).
+a partly-complete URL, which is by default the first entry in the `PkgGraph.base_urls`
+list. To use a different rendering website, use [`set_base_url`](@ref).
 """
-depgraph_url(pkgname) = begin
-    dotstr = deps_as_DOT(pkgname)
-    url = rendering_url[] * escapeuri(dotstr)
-end
+url(pkgname) = base_url[] * escapeuri(deps_as_DOT(pkgname))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,10 @@
 using PkgGraph
-using PkgGraph: depgraph, depgraph_url, rendering_urls, set_rendering_url,
-                deps_as_DOT, create_depgraph, to_DOT_str,
-                depgraph_local, is_dot_available
+using PkgGraph: depgraph, deps_as_DOT, url, is_dot_available
 using Test
 
 @testset "PkgGraph.jl" begin
 
-    @test create_depgraph("TOML") == [
+    @test depgraph("TOML") == [
         "TOML" => "Dates",
         "Dates" => "Printf",
         "Printf" => "Unicode"
@@ -26,11 +24,11 @@ using Test
     urlencoded = "digraph%20%7B%0A%20%20%20%20TOML%20-%3E%20Dates%0A%20%20%20%20" *
                  "Dates%20-%3E%20Printf%0A%20%20%20%20Printf%20-%3E%20Unicode%0A%7D%0A"
 
-    @test depgraph_url("TOML") == "https://dreampuf.github.io/GraphvizOnline/#" * urlencoded
+    @test url("TOML") == "https://dreampuf.github.io/GraphvizOnline/#" * urlencoded
 
-    set_rendering_url(last(rendering_urls))
+    PkgGraph.set_base_url(last(PkgGraph.base_urls))
 
-    @test depgraph_url("TOML") == "https://edotor.net/?engine=dot#" * urlencoded
+    @test url("TOML") == "https://edotor.net/?engine=dot#" * urlencoded
 
 
     if VERSION ≥ v"1.8"  # Julia v1.7 does not support error string matching
