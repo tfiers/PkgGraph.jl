f456f10174524dd06f5536a2da0e26c939fb22d2
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-10 02:59:58 +0000

    shorter names for Actions. doctest: instantiate

--- docs/doctest.jl
+++ docs/doctest.jl
@@ -1,3 +1,11 @@
-using Documenter, PkgGraph
+using Documenter
+using PkgGraph
+
+on_github = (get(ENV, "CI", "") == "true")  # This is set by GitHub itself.
+
+if on_github
+    using Pkg
+    Pkg.instantiate()
+end
 
 doctest(PkgGraph)
