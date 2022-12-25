eb124e3fd6d016c87973a18fbf41e9246d0d5643
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-25 11:55:54 +0100

    cleanup & improve `doc/scripts/preprocess_readme.md`

--- docs/scripts/preprocess_readme.jl
+++ docs/scripts/preprocess_readme.jl
@@ -1,6 +1,22 @@
 
 repo = "tfiers/PkgGraph.jl"
-ref = "main"
+gitref = "main"
+
+function make_links_absolute(src)
+    for pat in link_patterns
+        r(s) = replacement(s, pat)
+        src = replace(src, pat=>r)
+        # In `replace(str, pat=>r)`, the replacement `r`
+        # can be a:
+        #   - function `r(s)::AbstractString`
+        #   - regex's `SubstitutionString`
+        # ..but not yet a
+        #   - function `r(s)::SubstitutionString`
+        # (Maybe julia PR?)
+        # We hack it now by closing over the regex pattern.
+    end
+    src
+end
 
 link_patterns = [
     r"src *= *\"(\S+)\"",     # <img src="link">
@@ -8,38 +24,32 @@ link_patterns = [
     r"\[[^\]]+\]\((\S+)\)",   # [this](link)
     r"\[[^\]]+\]: +(\S+)",    # [this]: link
 ]
-url = r"^https?://\S+"
-is_url(s) = !isnothing(match(url, strip(s)))
-is_anchor(s) = startswith(strip(s), "#")
+#               ↪ \S: non-space character
 
 captured_link(m::RegexMatch) = only(m.captures)
-
-repo = strip(repo, '/')
-text_base_url = "https://github.com/$repo/blob/$ref/"
-nontext_base_url = "https://raw.githubusercontent.com/$repo/$ref/"
-
-absolute(rellink) = (istext(rellink) ? text_base_url * rellink
-                                     : nontext_base_url * rellink)
-
-istext(link) = !endswith(lowercase(link), nontext)
-
-nontext = r"\.svg|png|jpg|jpeg|gif|webp|mp4|mov|webm|zip|gz|tgz|pdf|pptx|docx|xlsx"
-# From https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/attaching-files
-
 
 replacement(substr, pattern) = begin
     link = captured_link(match(pattern, substr))
     if is_url(link) || is_anchor(link)
-        return substr
+        substr
     else
         println("  Making link [$link] absolute")
-        return replace(substr, link => absolute(link))
+        replace(substr, link => absolute(link))
     end
 end
 
-function make_links_absolute(src)
-    for pat in link_patterns
-        src = replace(src, pat => (s -> replacement(s, pat)))
-    end
-    return src
-end
+is_url(s) = !isnothing(match(r"^https?://\S+", strip(s)))
+
+is_anchor(s) = startswith(strip(s), "#")
+
+absolute(rellink) = (isbinary(rellink) ? binary_url(rellink)
+                                       : text_url(rellink)
+)
+isbinary(link) = endswith(lowercase(link), binary_ext)
+binary_ext = r"\.svg|png|jpg|jpeg|gif|webp|mp4|mov|webm|zip|gz|tgz|pdf|pptx|docx|xlsx"
+# From https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/attaching-files
+
+text_url(r) = (relpath=stripp(r); "https://github.com/$repo/blob/$gitref/$relpath")
+binary_url(r) = (relpath=stripp(r); "https://raw.githubusercontent.com/$repo/$gitref/$relpath")
+stripp(s) = strip(s, '/')
+repo = stripp(repo)
