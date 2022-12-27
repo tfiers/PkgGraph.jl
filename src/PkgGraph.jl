7858570047b9872ff38271832723838f09dfd60d
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-27 10:54:56 +0100

    PkgGraph module docstr: note that f's not exported

--- src/PkgGraph.jl
+++ src/PkgGraph.jl
@@ -4,6 +4,8 @@ Visualize the dependency graph of a Julia package.
 Use [`PkgGraph.open`](@ref) to view the graph in the browser,
 or [`PkgGraph.create`](@ref) to generate an image locally.
 
+(Note that these functions are not exported).
+
 See [`PkgGraph.Internals`](@ref) for more functions.
 """
 module PkgGraph
