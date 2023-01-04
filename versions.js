164cbbfb3c4f4bfa2ee56f20ce59430ec630cd5f
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-04 20:58:24 +0000

    Update versions.js

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
-  "dev",
   "v0.2",
+  "stable",
+  "dev",
 ];
-var DOCUMENTER_NEWEST = "dev";
-var DOCUMENTER_STABLE = "v0.2";
+var DOCUMENTER_NEWEST = "v0.2";
+var DOCUMENTER_STABLE = "stable";
