425b7802b7a7eaf209fe04dfd2f3a671efa5199f
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-05 16:16:51 +0100

    move `Internals` module def to `internals/Internals.jl` [close #51]

--- src/PkgGraph.jl
+++ src/PkgGraph.jl
@@ -10,56 +10,13 @@ See [`PkgGraph.Internals`](@ref) for more functions.
 """
 module PkgGraph
 
+using TOML
+using EzXML
+using URIs: escapeuri
+include("internals/Internals.jl")
 
-"""
-Namespace for the non-end-user functions in PkgGraph.
-
-For ease of experimentation, you can import these with
-```
-using PkgGraph.Internals
-```
-They are also imported in the main module, so they
-can be accessed as `PkgGraph.depgraph`, e.g.
-"""
-module Internals
-
-    using TOML
-    using Base: active_project
-    include("internals/depgraph.jl")
-    export depgraph,
-           packages_in_active_manifest,
-           should_be_included,
-           is_jll,
-           is_in_stdlib,
-           STDLIB,
-           vertices
-
-    include("internals/dot.jl")
-    export to_dot_str,
-           default_style
-
-    using EzXML
-    include("internals/svg.jl")
-    export add_darkmode
-
-    include("internals/local.jl")
-    export is_dot_available,
-           create_dot_image,
-           dotcommand,
-           output_path
-
-    using URIs: escapeuri
-    include("internals/online.jl")
-    export url,
-           webapps
-
-    using Base: @kwdef
-    include("internals/options.jl")
-    export Options
-end
-
-using .Internals
 using DefaultApplication
+using .Internals
 include("enduser.jl")
 
 # No package exports (no namespace pollution)
---
+++ src/internals/Internals.jl
@@ -0,0 +1,47 @@
+"""
+Namespace for the non-end-user functions in PkgGraph.
+
+For ease of experimentation, you can import these with
+```
+using PkgGraph.Internals
+```
+They are also imported in the main module, so they
+can be accessed as `PkgGraph.depgraph`, e.g.
+"""
+module Internals
+
+using TOML
+using Base: active_project
+include("depgraph.jl")
+export depgraph,
+       packages_in_active_manifest,
+       should_be_included,
+       is_jll,
+       is_in_stdlib,
+       STDLIB,
+       vertices
+
+include("dot.jl")
+export to_dot_str,
+       default_style
+
+using EzXML
+include("svg.jl")
+export add_darkmode
+
+include("local.jl")
+export is_dot_available,
+       create_dot_image,
+       dotcommand,
+       output_path
+
+using URIs: escapeuri
+include("online.jl")
+export url,
+       webapps
+
+using Base: @kwdef
+include("options.jl")
+export Options
+
+end
