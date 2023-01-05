3ede8d42e348de0456b88c2d2a26e84c990eefa5
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-05 15:56:50 +0100

    `o=default_options` for internals

--- src/internals/options.jl
+++ src/internals/options.jl
@@ -32,16 +32,18 @@ functions. I.e, no need to construct this object yourself.
     base_url  ::String          = first(webapps)
 end
 
+defaults = Options()
 
-depgraph(pkg, o::Options) =
+
+depgraph(pkg, o::Options = defaults) =
     depgraph(pkg; o.jll, o.stdlib)
 
-to_dot_str(pkg, o::Options) =
+to_dot_str(pkg, o::Options = defaults) =
     to_dot_str(
         depgraph(pkg, o);
         o.style,
         emptymsg = "($pkg has no dependencies)",
     )
 
-url(pkg, o::Options) =
+url(pkg, o::Options = defaults) =
     url(o.base_url, to_dot_str(pkg, o))
